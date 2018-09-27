e9d03907f7355bace2c66c25348b96503310aad3
KirillSemennikov2803 <semennikov1360@mail.ru>
2018-09-27 21:10:15 +0300

    Initial Commit

---
+++ cezar181_351_SemennikovKirill/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cezar181_351_SemennikovKirill
+//
+//  Created by Kirill on 27.09.2018.
+//  Copyright © 2018 Kirill. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
