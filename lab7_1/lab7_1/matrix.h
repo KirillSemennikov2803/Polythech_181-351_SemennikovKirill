543148dab811744752b6a2a2937bae37a438610f
KirillSemennikov2803 <43291924+KirillSemennikov2803@users.noreply.github.com>
2018-10-15 23:31:36 +0300

    23:31 15.10.2018

---
+++ lab7_1/lab7_1/matrix.h
@@ -0,0 +1,22 @@
+#pragma once
+class matrix
+{
+private:
+	int first_matr[10][10];
+	int stolb;
+	int strok;
+public:
+	bool correx(int a, int b);
+	bool input();
+	bool print();
+	bool summMatrix(matrix matr2);
+	bool multMatrix(matrix matr2);
+	int getElem(int st, int sl);
+	bool transp();
+	int getStrok();
+	int getStolb();
+
+	matrix();
+	~matrix();
+};
+
